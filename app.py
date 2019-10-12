a9c5d091c7dc7eaa8fd0dc97463ffdb654c8861d
unknown <agrimnautiyal1999@gmail.com>
2019-10-12 14:51:18 +0530

    Basic templates additions

--- app.py
+++ app.py
@@ -13,7 +13,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
         return render_template('index.html')
+@app.route('/about')
+def about_fun():
+        return render_template('about.html')
 
+@app.route('/login')
+def login_fun():
+        return render_template('login.html')
+
+@app.route('/signup')
+def signup_fun():
+        return render_template('signup.html')
 
 if __name__ == '__main__':
         app.run(debug=True)
