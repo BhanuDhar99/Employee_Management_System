808f26e7ce7a9ba4f9eabf05624e796f4862a4c1
unknown <agrimnautiyal1999@gmail.com>
2019-10-12 16:43:41 +0530

    Added support for admin login/signup

--- app.py
+++ app.py
@@ -60,11 +60,41 @@ def login_check_fun():
                 cur.execute('''SELECT Password from UserInfo where Email = ?''',(email,))
                 correct_password = cur.fetchall()
         if(correct_password[0][0]==password):
+                #redirect to user (non-admin) portal
                 return render_template('successful_user_login.html')
         else:
                 return render_template('unsuccessful_user_login.html')
 
+#route is  called to store admin signup info
+@app.route('/admin_store_signup_info', methods =['GET','POST'])
+def store_func_admin():
 
+        fname = request.form['Fname']
+        lname = request.form['Lname']
+        email = request.form['Email']
+        password = request.form['password']
+        phno = request.form['PhoneNumber']
+        org_name = request.form['Organization']
 
+        #now to store in our database
+        with sqlite3.connect('ems.db') as con:
+                cur = con.cursor()
+                cur.execute('''INSERT INTO AdminInfo VALUES(?,?,?,?,?,?)''',(fname,lname,email,password,phno,org_name))
+                con.commit
+        return render_template('signup_conf.html')
+#this route is used to check admin login
+@app.route('/admin_login_check', methods = ['GET', 'POST'])
+def login_check_fun_admin():
+        email = request.form['Email']
+        password = request.form['Password']
+        with sqlite3.connect('ems.db') as con:
+                cur = con.cursor()
+                cur.execute('''SELECT Password from AdminInfo where Email = ?''',(email,))
+                correct_password = cur.fetchall()
+        if(correct_password[0][0]==password):
+                #redirect to admin portal
+                return render_template('successful_user_login.html')
+        else:
+                return render_template('unsuccessful_user_login.html')
 if __name__ == '__main__':
         app.run(debug=True)
